cdf5be69aef4dcf1764ee4ad37d4bb4db69f6270
Chris Samarinas <algoprog@users.noreply.github.com>
2019-08-05 07:57:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     packages=find_packages(),
     install_requires=[
     	'Keras==2.2.4',
-		'Keras_Preprocessing==1.0.9',
-		'Flask_Cors==3.0.6',
-		'Flask==1.0.2',
-		'numpy==1.16.3',
-		'texttable==1.6.2',
-		'scikit_learn==0.21.3'
+	'Keras_Preprocessing==1.0.9',
+	'Flask_Cors==3.0.6',
+	'Flask==1.0.2',
+	'numpy==1.16.3',
+	'texttable==1.6.2',
+	'scikit_learn==0.21.3'
 	]
 )
